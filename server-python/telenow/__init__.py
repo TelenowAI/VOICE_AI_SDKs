9036b6c70ecda6b62da0923ea1c5bb11c8795378
Navin Kumar <34780375+Navinkr214@users.noreply.github.com>
2026-06-23 17:11:03 +0530

    updates versions

--- server-python/telenow/__init__.py
+++ server-python/telenow/__init__.py
@@ -4,4 +4,4 @@ from .client import ChatConversation, Telenow, TelenowError
 from .webhooks import verify_webhook
 
 __all__ = ["Telenow", "TelenowError", "ChatConversation", "verify_webhook", "custom_api"]
-__version__ = "0.1.3"
+__version__ = "0.1.4"
